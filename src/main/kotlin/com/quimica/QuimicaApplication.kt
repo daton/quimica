5bbafab9fb4833c5325f157107851e92f709c64b
daton <rapidclimate@gmail.com>
2021-10-01 11:01:22 -0500

    Initial commit

---
+++ src/main/kotlin/com/quimica/QuimicaApplication.kt
@@ -0,0 +1,11 @@
+package com.quimica
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuimicaApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuimicaApplication>(*args)
+}
---
+++ src/test/kotlin/com/quimica/QuimicaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.quimica
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuimicaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
